2524b81170672a0e493e4b3d62369a28c0b63b33
Xavier J. Cruz <xavvis308@gmail.com>
2024-05-01 15:46:55 -0400

    Betty style

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -9,19 +9,19 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-  size_t c = 0;
+        size_t c = 0;
   
-  if (tree == NULL)
-  {
-    return (0);
-  }
-  if (tree->left != NULL || tree->right != NULL)
-  {
-    c = 1;
-  }
+        if (tree == NULL)
+        {
+                return (0);
+        }
+        if (tree->left != NULL || tree->right != NULL)
+        {
+                c = 1;
+        }
   
-  c += binary_tree_nodes(tree->left);
-  c += binary_tree_nodes(tree->right);
+        c += binary_tree_nodes(tree->left);
+        c += binary_tree_nodes(tree->right);
   
-  return (c);
+        return (c);
 }
