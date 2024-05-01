a907f325eb74cc8c974ae6fb163e6bcb62de86e3
Xavier J. Cruz <xavvis308@gmail.com>
2024-05-01 15:49:11 -0400

    Betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,11 +9,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-  size_t d = 0;
-  while (tree && tree->parent)
-  {
-    tree = tree->parent;
-    d++;
-  }
-  return (d);
+        size_t d = 0;
+        while (tree && tree->parent)
+        {
+                tree = tree->parent;
+                d++;
+        }
+        return (d);
 }
